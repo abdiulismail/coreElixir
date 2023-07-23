c7ec1509b151389ac6a5acac043a86b184d0f3c9
abdullahi <abdiulsheikh@gmail.com>
2023-07-23 09:33:46 +0300

    modules

--- _build/dev/lib/coreelixir/ebin/coreelixir.app
+++ _build/dev/lib/coreelixir/ebin/coreelixir.app
@@ -2,7 +2,8 @@
              [{optional_applications,[]},
               {applications,[kernel,stdlib,elixir,logger,ex_doc]},
               {description,"coreelixir"},
-              {modules,['Elixir.Coreelixir','Elixir.Mymath','Elixir.Mynumbes',
+              {modules,['Elixir.Coreelixir','Elixir.Introduction',
+                        'Elixir.Mymath','Elixir.Mymodules','Elixir.Mynumbes',
                         'Elixir.Myvariable']},
               {registered,[]},
               {vsn,"0.1.0"}]}.
---
+++ lib/1. Beginner/modules/modules.ex
@@ -0,0 +1,47 @@
+
+defmodule MyModules do
+  @moduledoc """
+   a module is a collection of functions.
+   every elixir function must be defined inside a module
+
+   elixir comes with a standard library that provides many useful modules such IO module
+   can be used to do various I/O operations. The function puts from the IO module can be used to print a message to the screen
+
+   to call a function of a module , you use the syntax
+   modulename.function_name(args)
+
+   to define your own module , you use defmodule construct.
+   inside defmodule , you define functions using def construct
+
+   a single source file may contain multiple modules
+
+        defmodule Module1 do
+            ...
+        end
+
+        defmodule Module2 do
+        ...
+        end
+
+  a module name must start with a capital lette and is usually written in camel case
+  a module name can consist of alphanumerics, underscores and dot character
+
+      defmodule Geometry.Rectangle do
+      ...
+      end
+
+  you can also nest modules inside another module
+
+        defmodule Geometry do
+            defmodule Rectangle do
+      ...   end
+      ..end
+
+
+  """
+
+  def rectangle_area(a,b) do
+    a * b
+  end
+
+end

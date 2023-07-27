78d0faac6d4d9f6307426d7957be4340ed658983
abdullahi <abdiulsheikh@gmail.com>
2023-07-27 21:21:48 +0300

    list challenges cont..

--- lib/1. Beginner/4. lists/challenges.ex
+++ lib/1. Beginner/4. lists/challenges.ex
@@ -1,5 +1,16 @@
 defmodule ListChallenges do
 
+  # function to calculate the length of a list
+  def checklength([]),do: []
+  def checklength(mylisy) do
+    length(mylisy)
+  end
+
+  # function to check if a list is empty
+  def checkempty([]),do: IO.puts("empty list")
+  def checkempty(_),do: IO.puts("list not empty")
+
+
   # function that reverse a list
   def reverse([],rv), do: rv
   def reverse([head | tail] , rv) do

22ed82512d440c2cebe8dba8dd9da44f161e35a5
abdullahi <abdiulsheikh@gmail.com>
2023-07-28 21:29:16 +0300

    exploring list functions..

--- lib/1. Beginner/4. lists/challenges.ex
+++ lib/1. Beginner/4. lists/challenges.ex
@@ -1,19 +1,19 @@
 defmodule ListChallenges do
-
   # function to calculate the length of a list
-  def checklength([]),do: []
+  def checklength([]), do: []
+
   def checklength(mylisy) do
     length(mylisy)
   end
 
   # function to check if a list is empty
-  def checkempty([]),do: IO.puts("empty list")
-  def checkempty(_),do: IO.puts("list not empty")
-
+  def checkempty([]), do: IO.puts("empty list")
+  def checkempty(_), do: IO.puts("list not empty")
 
   # function that reverse a list
-  def reverse([],rv), do: rv
-  def reverse([head | tail] , rv) do
-    reverse(tail, [head | rv])
+  def reverse([], reversed_list), do: reversed_list
+
+  def reverse([head | tail], reversed_list) do
+    reverse(tail, [head | reversed_list])
   end
 end
--- lib/1. Beginner/4. lists/lists.ex
+++ lib/1. Beginner/4. lists/lists.ex
@@ -86,6 +86,8 @@ defmodule MyList do
               [:new_element, 5, :value, true]
 
 
+    so many functions to work with in lists are found in Enum module
+
   """
 
   def mylist do
@@ -119,7 +121,13 @@ defmodule MyList do
     List.insert_at(prime_numbers, -1, 1)
   end
 
-  # concatenate two list
+  # insertion using pop-like operation (appending new item to list)
+  def insert do
+    prime_numbers = [1, 2, 3, 5, 7]
+    [:new_element | prime_numbers]
+  end
+
+  # concatenate two list using ++ or subract using --
   def conc do
     [1, 2, 4] ++ [5, 6, 7]
   end
@@ -143,4 +151,119 @@ defmodule MyList do
     # IO.puts("#{new_elemt}")
     # transfrm(tail)
   end
+
+  # delete an element from a list
+  #List.delete([],e) , delete a given element from a list, returns a new list without the element
+  # if the element occurs more than just once in the list, just the first occurence is removed
+
+  def dele([]), do: []
+  def dele(lst) do
+    List.delete(lst,2)
+    # iex(1)> list = [1,2,3,4,5]
+    # [1, 2, 3, 4, 5]
+    # iex(2)> MyList.dele(list)
+    # [1, 3, 4, 5]
+    # iex(3)>
+  end
+
+  # delete a specific value using index of that value
+  def deleat([]), do: []
+  def deleat(lst) do
+    List.delete_at(lst,2)
+    # iex(1)> list = [1,2,3,4,5]
+    # [1, 2, 3, 4, 5]
+    # iex(2)> MyList.dele(list)
+    # [1, 2, 4, 5]
+    # iex(3)>
+  end
+
+  # duplicate(elem, n) - duplicates the given element n times in a list
+  # n is an integer greater than or equal to 0
+  # if n is 0, an empty list is returned
+  def dupli([]),do: []
+  def dupli(list) do
+    List.duplicate(list,3)
+    # iex(1)> list = [1,2,3,4,5]
+    # [1, 2, 3, 4, 5]
+    # iex(2)> MyList.dupli(list)
+    # [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5]]
+    # iex(3)>
+  end
+
+  # first(list, default \\ nil) - returns the first element in list or default if list is empty
+  def first([]),do: []
+  def first(list) do
+    List.first(list)
+    # iex(1)> list = [1,2,3,4,5]
+    # [1, 2, 3, 4, 5]
+    # iex(2)> MyList.dupli(list)
+    # [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5]]
+    # iex(3)>
+  end
+
+  # flatten(list) - flattens the given list of nested list
+  def flaten([]),do: []
+  def flaten(list) do
+    List.flatten(list)
+    # iex(14)> list = [1,2,[3,4,[5,6,[7,8,9]]]]
+    # [1, 2, [3, 4, [5, 6, ~c"\a\b\t"]]]
+    # iex(15)> MyList.flaten(list)
+    # [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
+
+  # flatten(list, tail) - flattens the given list of nested lists,
+  # the list tail will be added at the end of the flattened list
+  # empty list elements from the list are discarded, but not the ones from the tail
+  def flaten2([],_),do: []
+  def flaten2(list, tail) do
+    List.flatten(list,tail)
+    # iex(21)> list = [1,2,[3,4,[5,6,[7,8,9]]]]
+    # [1, 2, [3, 4, [5, 6, ~c"\a\b\t"]]]
+    # iex(22)> MyList.flaten2(list,[9,[],10])
+    # [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, [], 10]
+  end
+
+  # foldl(list,acc, fun) - folds(reduces) the given list from the left with a function
+  # comeback to this when understood ->
+
+  # improper?(list) - returns true if list is an improper list, otherwise returns false
+  def impropa(list) do
+    List.improper?(list)
+    # List.improper?([1, 2 | 3])
+    # true
+
+    # List.improper?([1, 2, 3])
+    # false
+  end
+
+
+  # keydelete(list, key, position) - receives a list of tuples and deletes the first tuples where the element at position matches the given key
+  def keyd(list,x,no) do
+    List.keydelete(list,x,no)
+      # List.keydelete([a: 1, b: 2], :a, 0)
+      # [b: 2]
+
+      # List.keydelete([a: 1, b: 2], 2, 1)
+      # [a: 1]
+
+      # List.keydelete([a: 1, b: 2], :c, 0)
+      # [a: 1, b: 2]
+  end
+
+  # keyfind(list,key,position,default \\ nil) - receives a list of tuples and returns the tuple where the element at position in the tuple matches the given key
+  # if no match is found, default is returned
+  def keyfnd(list,key,pos) do
+    List.keyfind(list,key,pos)
+      # List.keyfind([a: 1, b: 2], :a, 0)
+      # {:a, 1}
+
+      # List.keyfind([a: 1, b: 2], 2, 1)
+      # {:b, 2}
+
+      # List.keyfind([a: 1, b: 2], :c, 0)
+      # nil
+  end
+
+  
+
 end

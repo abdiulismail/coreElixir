718fc2cce26553fff192e955725984f15c089c39
abdullahi <abdiulsheikh@gmail.com>
2023-08-03 16:13:25 +0300

    guards

---
+++ lib/13.5 guards/guards.ex
@@ -0,0 +1,21 @@
+defmodule Guards do
+  @moduledoc """
+    guards are extensions of the basic pattern matching mechanism.
+    they allow you to state additional broader expectations that must be satisfied for the entire pattern to match
+
+    a guard can be specified by providing the when clause after the arguments list
+
+    def test(x) when x < 0 do
+      :negative
+    end
+
+    the guars is a logical expression that places further conditions on a clause.
+
+    def test(x) when is_number and x > 0 do
+       :positive
+    end
+
+    
+
+  """
+end
